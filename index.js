4d6358d43f0a1a4b2191026ed0e034614413a5fc
Halim Qarroum <hqm.post@gmail.com>
2018-04-18 02:38:53 +0200

    Removed unused code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const diff     = require('ansi-diff-stream');
  * Called back when promises are enqueued or have been
  * executed on the pool.
  */
-const hook = (type, opts, bars, pool, out) => (e) => {
+const hook = (opts, pool, out) => (e) => {
   let string = 'Balance of promises between executors:';
   const array = pool.promises();
   for (let i = 0; i < array.length; ++i) {
@@ -18,7 +18,6 @@ const hook = (type, opts, bars, pool, out) => (e) => {
 };
 
 module.exports = (pool, opts) => {
-  const bars = [];
   const out  = diff();
 
   opts.total = +opts.total / (pool.opts.size > 1 ? pool.opts.size - 1 : 1);
@@ -26,18 +25,11 @@ module.exports = (pool, opts) => {
     throw new Error('An instance to a promise pool is required');
   }
 
-  // Initializing the progress bars.
-  const array = pool.promises();
-  for (let i = 0; i < array.length; ++i) {
-    bars.push({ count: array[i].load, progress: progress(opts), idx: array[i].idx });
-  }
-
   // Subscribing to lifecycle events on the pool.
-  const before = hook('before', opts, bars, pool, out);
-  const after  = hook('after', opts, bars, pool, out);
-  pool.on('before.enqueue.each', before).on('after.each', after);
+  const h = hook(opts, pool, out);
+  pool.on('before.enqueue.each', h).on('after.each', h);
   
   // Allowing to stop the exection.
-  out.detach = () => pool.removeListener('before.enqueue.each', before).removeListener('after.each', after);
+  out.detach = () => pool.removeListener('before.enqueue.each', h).removeListener('after.each', h);
   return (out);
 };
